09df976519c50768662c1d1a62a6572fca30a358
Vesko <veskoap@gmail.com>
2023-01-09 03:23:06 -0500

    upd env

--- src/sanity/lib/fetchProjects.ts
+++ src/sanity/lib/fetchProjects.ts
@@ -1,7 +1,7 @@
 import { Project } from "../../page/types";
 
 export const fetchProjects = async () => {
-  const res = await fetch(`http://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/getProject`);
+  const res = await fetch(`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/getProject`);
 
   const data = await res.json();
   const projects: Project[] = data.projects;
--- src/sanity/lib/fetchSkills.ts
+++ src/sanity/lib/fetchSkills.ts
@@ -1,7 +1,7 @@
 import { Skill } from "../../page/types";
 
 export const fetchSkills = async () => {
-  const res = await fetch(`http://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/getSkills`);
+  const res = await fetch(`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/getSkills`);
 
   const data = await res.json();
   const skills: Skill[] = data.skills;
