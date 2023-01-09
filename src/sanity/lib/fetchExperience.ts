355109f33161e0c4d6157dcb994c58e312198569
Vesko <veskoap@gmail.com>
2023-01-09 02:30:53 -0500

    updated fetchapi url

--- src/sanity/lib/fetchExperience.ts
+++ src/sanity/lib/fetchExperience.ts
@@ -1,7 +1,7 @@
 import { Experience } from "../../page/types";
 
 export const fetchExperiences = async () => {
-  const res = await fetch('http://localhost:3000/api/getExperience');
+  const res = await fetch(`${process.env.VERCEL_URL}/api/getExperience`);
 
   const data = await res.json();
   const experiences: Experience[] = data.experiences;
