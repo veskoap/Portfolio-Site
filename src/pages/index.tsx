9b876b36b59593ebdabcf3126db257d3e15fe000
Vesko <veskoap@gmail.com>
2023-01-09 03:47:09 -0500

    created local env

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import type { GetStaticProps } from "next";
+import type { GetStaticProps } from "next"
 import Head from "next/head";
 
 import About from "../components/About";
--- src/sanity/lib/fetchSocials.ts
+++ src/sanity/lib/fetchSocials.ts
@@ -1,7 +1,7 @@
 import { Social } from "../../page/types";
 
 export const fetchSocials = async () => {
-  const res = await fetch(`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/getSocials`); 
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSocials`); 
 
   const data = await res.json();
   const socials: Social[] = data.socials;
