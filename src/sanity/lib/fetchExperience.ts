45a88ffc2575bed9d104766f56e9bb7d4b1cae07
Vesko <veskoap@gmail.com>
2023-01-09 07:05:11 -0500

    updated env var

--- src/sanity/lib/fetchExperience.ts
+++ src/sanity/lib/fetchExperience.ts
@@ -1,12 +1,12 @@
-import { Experience } from "../../page/types";
+import {Experience} from '../../page/types'
 
 export const fetchExperiences = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getExperience`);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getExperience`)
 
-  const data = await res.json();
-  const experiences: Experience[] = data.experiences;
+  const data = await res.json()
+  const experiences: Experience[] = data.experiences
 
-  console.log("fetching", experiences)
+  console.log('fetching', experiences)
 
-  return experiences;
-};
+  return experiences
+}
