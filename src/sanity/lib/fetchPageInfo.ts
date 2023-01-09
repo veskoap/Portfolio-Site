94100375372be901c7335de1baf5b78100716e81
Vesko <veskoap@gmail.com>
2023-01-09 02:44:25 -0500

    updated vercel url in apis

--- src/sanity/lib/fetchPageInfo.ts
+++ src/sanity/lib/fetchPageInfo.ts
@@ -1,7 +1,7 @@
 import { PageInfo } from "../../page/types";
 
 export const fetchPageInfo = async () => {
-  const res = await fetch(`${process.env.VERCEL_URL}/api/getPageInfo`);
+  const res = await fetch(`https://${process.env.VERCEL_URL}/api/getPageInfo`);
 
   const data = await res.json();
   const pageInfo: PageInfo = data.pageInfo;
--- src/sanity/lib/fetchSkills.ts
+++ src/sanity/lib/fetchSkills.ts
@@ -1,7 +1,7 @@
 import { Skill } from "../../page/types";
 
 export const fetchSkills = async () => {
-  const res = await fetch(`${process.env.VERCEL_URL}/api/getSkills`);
+  const res = await fetch(`https://${process.env.VERCEL_URL}/api/getSkills`);
 
   const data = await res.json();
   const skills: Skill[] = data.skills;
