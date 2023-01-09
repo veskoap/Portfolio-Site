8df6b1056237d2d8b7e06bdf6d69986af7be91b2
Vesko <veskoap@gmail.com>
2023-01-09 05:09:50 -0500

    updated env var

--- next.config.mjs
+++ next.config.mjs
@@ -10,5 +10,9 @@ const config = {
 
   reactStrictMode: true,
 }
-
+module.exports = {
+  env: {
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+  },
+};
 export default config
--- src/sanity/lib/fetchExperience.ts
+++ src/sanity/lib/fetchExperience.ts
@@ -1,7 +1,7 @@
 import { Experience } from "../../page/types";
 
 export const fetchExperiences = async () => {
-  const res = await fetch(`https://.../api/getExperience`);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getExperience`);
 
   const data = await res.json();
   const experiences: Experience[] = data.experiences;
