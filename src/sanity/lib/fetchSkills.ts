6e1085e47861c6613662d2e0ba37172cf88808dc
Vesko <veskoap@gmail.com>
2023-01-09 04:43:04 -0500

    updated env var

--- src/sanity/lib/fetchSkills.ts
+++ src/sanity/lib/fetchSkills.ts
@@ -1,7 +1,7 @@
 import { Skill } from "../../page/types";
 
 export const fetchSkills = async () => {
-  const res = await fetch('https://.../api/getSkills');
+  const res = await fetch(`https://.../api/getSkills`);
 
   const data = await res.json();
   const skills: Skill[] = data.skills;
