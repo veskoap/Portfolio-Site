588fd09a7f8a66e38c3a3a6ec0654dcbc4d29c0f
Vesko <veskoap@gmail.com>
2023-01-10 17:16:16 -0500

    upd css

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -38,7 +38,7 @@ export default function About({pageInfo}: Props) {
             alt="Another picture of me"
             width={200}
             height={200}
-            className=" flex max-w-[240px] h-48 w-48 md:h-56 md:w-56 xl:h-64 xl:w-64 flex-shrink-0 rounded-full scroll -pb-10 md:max-w-sm md:rounded"
+            className=" flex max-w-[240px] h-36 w-36 md:h-56 md:w-56 xl:h-64 xl:w-64 flex-shrink-0 rounded-full scroll -pb-10 md:max-w-sm md:rounded"
           />
         </motion.div>
 
