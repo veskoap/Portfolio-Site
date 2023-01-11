cb8658ea5da0c414b5947700e6f6d85a30786696
Vesko <veskoap@gmail.com>
2023-01-10 20:01:44 -0500

    updated css to make site more responsive on mobile

--- src/components/WorkExperience.tsx
+++ src/components/WorkExperience.tsx
@@ -18,7 +18,7 @@ export default function WorkExperience({experiences}: Props) {
         Experience
       </h3>
 
-      <div className="w-screen flex space-x-5 mt-20 xl:mt-32 overflow-x-scroll p-10 snap-x snap-mandatory h-auto pb-16 xl:pb-20 scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#892CDC]/80">
+      <div className="w-screen flex space-x-5 mt-16 md:mt-20 xl:mt-32 overflow-x-scroll p-10 snap-x snap-mandatory h-auto pb-16 xl:pb-20 scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#892CDC]/80">
         {experiences?.map((experience) => (
           <ExperienceCard key={experience._id} experience={experience} />
         ))}
