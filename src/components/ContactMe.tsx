a4105b9caa7b0bb2ade8f127597feb0d5b672f0c
Vesko <veskoap@gmail.com>
2023-01-10 21:29:53 -0500

    updated css to make site more responsive on mobile

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -23,35 +23,35 @@ function ContactMe({pageInfo}: Props) {
 
   return (
     <div className="h-screen flex relative flex-col text-center max-w-7xl px-10 justify-evenly mx-auto items-center">
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">contact</h3>
-      <div className="flex flex-col space-y-10">
+      <h3 className="absolute top-16 md:top-20 xl:top-24 uppercase tracking-[10px] lg:tracking-[20px] text-gray-500 text-2xl">contact</h3>
+      <div className="flex flex-col space-y-5 md:space-y-10 mt-10">
         <h4 className="text-2xl lg:text-4xl font-semibold text-center">
           Feel free to reach out.{' '}
           <span className="underline decoration-[#892CDC]/50">Lets Chat</span>
         </h4>
 
-        <div className="space-y-10">
+        <div className="space-y-2 md:space-y-10">
           <div className="flex items-center space-x-5 justify-center">
-            <PhoneIcon className="text-[#892CDC] h-7 w-7 animate-pulse" />
-            <p className="text-xl md:text-2xl">{pageInfo?.phoneNumber}</p>
+            <PhoneIcon className="text-[#892CDC] h-5 w-5 md:h-7 md:w-7 animate-pulse" />
+            <p className="text-lg md:text-xl lg:text-2xl">{pageInfo?.phoneNumber}</p>
           </div>
 
           <div className="flex items-center space-x-5 justify-center">
-            <EnvelopeIcon className="text-[#892CDC] h-7 w-7 animate-pulse" />
-            <p className="text-xl md:text-2xl">{pageInfo?.email}</p>
+            <EnvelopeIcon className="text-[#892CDC] h-5 w-5 md:h-7 md:w-7 animate-pulse" />
+            <p className="text-lg md:text-xl lg:text-2xl">{pageInfo?.email}</p>
           </div>
 
           <div className="flex items-center space-x-5 justify-center">
-            <MapPinIcon className="text-[#892CDC] h-7 w-7 animate-pulse" />
-            <p className="text-xl md:text-2xl">{pageInfo?.address}</p>
+            <MapPinIcon className="text-[#892CDC] h-5 w-5 md:h-7 md:w-7 animate-pulse" />
+            <p className="text-lg md:text-xl lg:text-2xl">{pageInfo?.address}</p>
           </div>
         </div>
 
         <form
           onSubmit={handleSubmit(onSubmit)}
-          className="flex flex-col w-fit mx-auto flex-wrap space-y-2"
+          className="flex flex-col w-auto mx-auto flex-wrap space-y-2"
         >
-          <div className="flex space-x-2">
+          <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 space-x-0 md:space-x-2">
             <input {...register('name')} placeholder="Name" className="contactInput" type="text" />
             <input
               {...register('email')}
@@ -69,7 +69,7 @@ function ContactMe({pageInfo}: Props) {
           />
 
           <textarea {...register('message')} placeholder="Message" className="contactInput" />
-          <button type="submit" className="bg-[#892CDC] py-5 px-10 rounded-md font-bold text-lg">
+          <button type="submit" className="bg-[#892CDC] py-2 px-5 md:py-5 md:px-10 rounded-md font-bold text-lg">
             Submit
           </button>
         </form>
